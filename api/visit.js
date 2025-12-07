07adcf91592f1aa7189b4bba19d974bf3e319ae7
chillead <fres1x43@gmail.com>
2025-12-07 12:58:14 +0500

    Update visit.js

--- api/visit.js
+++ api/visit.js
@@ -1,18 +1,17 @@
 import crypto from "crypto";
 
-let visitors = new Set();
+const seen = new Set();
 let count = 0;
 
 export default function handler(req, res) {
   const ip =
     req.headers["x-forwarded-for"]?.split(",")[0] ||
-    req.socket.remoteAddress ||
     "unknown";
 
   const hash = crypto.createHash("sha256").update(ip).digest("hex");
 
-  if (!visitors.has(hash)) {
-    visitors.add(hash);
+  if (!seen.has(hash)) {
+    seen.add(hash);
     count++;
   }
 
